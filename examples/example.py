5bfcc42d9cadf85e2b155e88b1cdda5af5bf5499
Marianna Nezhurina <marianna@cpu128-dy-c6i-32xlarge-2.hpc-1click-ingress-east.pcluster>
2023-10-23 15:24:23 +0000

    upd example

--- examples/example.py
+++ examples/example.py
@@ -1,12 +1,23 @@
-from pdf_extraction import process_doc, pdf_extractor
+from main import process_doc, pdf_extractor
 import time
 
-file_list = "/admin/home-marianna/dcnlp/pdf_pipeline/sample.txt"
-output_folder = "/admin/home-marianna/dcnlp/pdf_pipeline/test_result"
+file_list = "test_books_100.txt"
 
-s = time.time()
+processes_counts = [1, 5, 10]
 
-pdf_extractor(file_list=file_list, output_folder=output_folder, input_format="txt", output_format="txt")
+for processes_count in processes_counts:
+    output_folder = f"/admin/home-marianna/dcnlp/pdf_pipeline/test_result_{processes_count}_proc"
 
-e = time.time()
-print(e - s)
+    s = time.time()
+
+    pdf_extractor(
+        file_list=file_list, 
+        output_folder=output_folder, 
+        input_format="csv", 
+        output_format="parquet", 
+        file_col='url',
+        processes_count=processes_count,
+        )
+
+    e = time.time()
+    print(e - s)
